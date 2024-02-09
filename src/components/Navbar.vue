a68e10830b1c620e018bbfe6c2ed717a2217f2a5
Joji Shiotsuki <149657863+jojiShiotsuki@users.noreply.github.com>
2024-02-09 15:17:35 +0800

    First commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+	 <div class="navbar">
+        <RouterLink to="/" class="active">Home</RouterLink>
+        <RouterLink href="#about">About</RouterLink>
+    </div>
+</template>
